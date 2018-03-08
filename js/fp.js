4591cc8805e4580e27ba768f25c43ac32ade2182
Catherine Blake Smith <34329444+thecbsnetwork@users.noreply.github.com>
2018-03-08 01:31:14 -0800

    Initializes template version for final project
    Cannot figure out how to correct sticky nav for desktop view. ):

---
+++ js/fp.js
@@ -0,0 +1,23 @@
+window.onscroll = function() {stickyNav()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("navbar");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
